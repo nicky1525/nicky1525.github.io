a2e573426f1f63ae5dafbb74a88837b3fc5eabf7
Nicole <nicky_1525@hotmail.it>
2014-03-06 11:21:32 +0100

    Firefox issue fixed

--- js/script.js
+++ js/script.js
@@ -1,51 +1,4 @@
 var previousScroll = 0,
-      headerOrgOffset = $('.presentation').height();
-
-
-  $(window).scroll(function() {
-      var currentScroll = $(this).scrollTop();
-      
-      if(currentScroll > headerOrgOffset) {
-          if (currentScroll <= previousScroll) {
-              $('.navbar-hidden').animate({
-                   top: '-70px'      //Change to Height of navbar
-              }, 1000);               //Mess with animate time
-          } else {
-              $('.navbar-hidden').animate({
-                   top: '0px'
-              },1000);
-              $('.navbar-hidden').addClass('fixed');
-          }
-      } else {
-           $('.navbar-hidden').removeClass('fixed');   
-      }
-      previousScroll = currentScroll;
-  });
-
-
-window.pop = function pop() {
-  var counter = 60;
-
-  var box = document.createElement('div');
-  var label = document.createElement('span');
-  label.innerText = "Thank you! I got your message and will reply to you soon!";
-  box.appendChild(label);
-
-  // Position box and label as you wish.
-
-  function tick() {
-    counter--;
-    if (counter == 0) {
-      window.close();
-    } else {
-      label.innerText = counter;
-      setTimeout(tick, 1000);
-    }
-  }
-
-  setTimeout(tick, 1000);
-}
-var previousScroll = 0,
       headerOrgOffset = $('#work').height() + $('.presentation').height();
 
 
@@ -59,7 +12,7 @@ var previousScroll = 0,
               }, 1000);               //Mess with animate time
           } else {
               $('.navbar-hidden').animate({
-                   top: '0px'
+                   top: '0'
               },1000);
               $('.navbar-hidden').addClass('fixed');
           }
