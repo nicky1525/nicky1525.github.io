aac3284d3a3053ed8612c63f7363c64992b801bc
nicky1525 <nicky_1525@hotmail.it>
2014-11-17 11:03:55 +0000

    Using PHPMailer

--- phpMailer.php
+++ phpMailer.php
@@ -13,8 +13,8 @@
 
 	               // SMTP username
 	$mail->Password = $password;                           // SMTP password
-	// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-	// $mail->Port = 465;
+	$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 465;
 	$recipient_email = "nicole@delafeld.it";
 	$sender_name = isset($_POST['name']) ? trim($_POST['name']) : '';
 	$sender_email = isset($_POST['email']) ? trim($_POST['email']) : '';
